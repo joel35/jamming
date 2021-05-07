369080ec503dbf89668b6cdd1079504fef309825
joel35 <system@icenilabs.com>
2021-05-07 18:04:41 +0100

    check spotify url (unfinished)

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -5,18 +5,15 @@ const Spotify = {
         if (accessToken) {
             return accessToken
         } else if (this.regex.every(re => this.searchUrl(re))) {
-            accessToken = this.searchUrl(this.regex.accessToken);
-            const expiresIn = this.searchUrl(this.regex.expiresIn);
+            accessToken = this.searchUrl(this.regex[0][1]);
+            const expiresIn = this.searchUrl(this.regex[1][1]);
 
             window.setTimeout(() => accessToken = null, expiresIn * 1000);
             window.history.pushState('Access Token', null, '/');
         }
     },
 
-    regex: {
-        accessToken: '/access_token=([^&]*)/',
-        expiresIn: '/expires_in=([^&]*)/',
-    },
+    regex: [/access_token=([^&]*)/, /expires_in=([^&]*)/],
 
     searchUrl(re) {
         return window.location.href.match(re)
