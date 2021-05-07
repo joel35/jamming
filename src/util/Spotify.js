df040d8d330f59ff531c7490909928d13ec13e79
joel35 <system@icenilabs.com>
2021-05-07 17:44:49 +0100

    check spotify url (unfinished)

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -1,18 +1,22 @@
-const accessToken = null;
+let accessToken = null;
 
 const Spotify = {
     getAccessToken() {
         if (accessToken) {
             return accessToken
-        } else if (window.location.href.match([
-            /access_token=([^&]*)/,
-            /expires_in=([^&]*)/
-        ])) {
+        } else if (this.checkUrl(/access_token=([^&]*)/) && this.checkUrl(/expires_in=([^&]*)/)) {
 
+            accessToken = this.checkUrl(/access_token=([^&]*)/);
+            const expiresIn = this.checkUrl(/expires_in=([^&]*)/);
+            window.setTimeout(() => accessToken = null, expiresIn * 1000);
+            window.history.pushState('Access Token', null, '/');
         }
 
 
     },
+    checkUrl(regex) {
+        return window.location.href.match(regex)
+    }
 };
 
 export default Spotify
