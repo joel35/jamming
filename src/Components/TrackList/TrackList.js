f4c24c17c1b2d45364fb169097cd3364e4da0f17
joel35 <system@icenilabs.com>
2021-05-10 11:43:02 +0100

    added return to function def

--- src/Components/TrackList/TrackList.js
+++ src/Components/TrackList/TrackList.js
@@ -3,17 +3,18 @@ import './TrackList.css';
 import Track from "../Track/Track";
 
 function TrackList(props) {
+    // console.log(props)
     return (
         <div className="TrackList">
-            {props.tracks.map((track) => (
-                <Track
+            {props.tracks.map((track) => {
+                return <Track
                     key={track.id}
                     track={track}
                     onAdd={props.onAdd}
                     onRemove={props.onRemove}
                     isRemoval={props.isRemoval}
                 />
-            ))}
+            })}
         </div>
     );
 }
